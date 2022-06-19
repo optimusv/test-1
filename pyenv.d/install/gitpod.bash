9ecf01c8535a6445e23a24056e1ff9d16c23e09f
AXON <axonasif@gmail.com>
2022-06-19 20:00:12 +0000

    up

--- pyenv.d/install/gitpod.bash
+++ pyenv.d/install/gitpod.bash
@@ -1,5 +1,5 @@
 GP_PYENV_MIRROR="/workspace/.pyenv_mirror";
 ORIG_PYENV_ROOT="$PYENV_ROOT";
-PYENV_ROOT="$GP_PYENV_MIRROR/fakeroot"; # Hijack PYENV_ROOT temporarily
+PYENV_ROOT="$GP_PYENV_MIRROR/fakeroot"; # Temporarily hijack PYENV_ROOT
 
 after_install "ln -s $PYENV_ROOT/versions/$DEFINITION $ORIG_PYENV_ROOT/versions"; # Symlink to the original PYENV_ROOT under $HOME
--- pyenv.d/which/gitpod.bash
+++ pyenv.d/which/gitpod.bash
@@ -2,5 +2,8 @@ if test ! -x "$PYENV_COMMAND_PATH"; then {
     GP_PYENV_MIRROR="/workspace/.pyenv_mirror";
     for bin in "${GP_PYENV_MIRROR}/site/"*"/bin/$PYENV_COMMAND"; do {
         PYENV_COMMAND_PATH="$bin";
+        if test -x "$PYENV_COMMAND_PATH"; then {
+            break;
+        } fi
     } done
 } fi
